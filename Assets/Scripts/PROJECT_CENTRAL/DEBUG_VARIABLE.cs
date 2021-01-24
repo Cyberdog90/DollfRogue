b6998f13b7a63f9ca28b57af9287a5f0163acf5f
CYBAiGNU <43635561+Cyberdog90@users.noreply.github.com>
2021-01-25 01:23:09 +0900

    add debug mode

---
+++ Assets/Scripts/PROJECT_CENTRAL/DEBUG_VARIABLE.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class DEBUG_VARIABLE : MonoBehaviour
+{
+    private const string DEBUG_PASS = "457700";
+    private bool DEBUG_FRAG;
+
+    public string DEBUGPASS {
+        get => DEBUG_PASS;
+    }
+
+    public bool DEBUGFRAG {
+        get => DEBUG_FRAG;
+        set => DEBUG_FRAG = value;
+    }
+}
---
+++ Assets/Scripts/TitleMenu/TitleMenu_Debug.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+public class TitleMenu_Debug : MonoBehaviour {
+    private string test = "";
+    private readonly DEBUG_VARIABLE _dV = new DEBUG_VARIABLE();
+    void Start() {
+    }
+
+    void Update() {
+        if (Input.anyKeyDown) {
+            foreach (KeyCode code in Enum.GetValues(typeof(KeyCode))) {
+                if (Input.GetKeyDown(code)) {
+                    var tmp = code.ToString().Replace("Alpha", "");
+                    test += tmp;
+                    Debug.Log(test);
+                }
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightControl)) test = "";
+
+        if (test == _dV.DEBUGPASS && !_dV.DEBUGFRAG) {
+            Debug.Log("立ったまま死ね!");
+            _dV.DEBUGFRAG = true;
+        }
+    }
+}
